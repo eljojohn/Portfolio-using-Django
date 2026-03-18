8254ef78fc558d794d4c0bfbb8d6c238158a10b1
Eljo John <eljojohnsachu@gmail.com>
2026-03-19 03:26:02 +0530

    fix static files

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -106,8 +106,8 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Folder where collectstatic will place production-ready files
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
+#STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Use WhiteNoise for serving static files in production
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
